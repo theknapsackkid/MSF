711e27a4fc4f3c166da59cbad438ef3950cce00f
Sam Hutchinson <Sam Hutchinson>
2016-09-07 01:23:59 -0400

    Updated user controller, check uniquness of name and added get

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,20 +6,30 @@ var utils = require("../utils")
 var router = express.Router();
 router.route("/")
     .post(function(req,res){
-        var user = new User();
-        user.userName = req.body.userName;
-        user.password = req.body.password;
-        if(req.body.nickName)
-            user.nickName = req.body.nickname
-        
-        user.save(function(err){
-            if(err)
-                res.send(err);
-            res.json(user);
+        User.find({ userName: req.body.userName}, function(err, users) {
+            if(users.length > 0) {
+                res.send({error: "No"});
+                return;
+            }
+            var user = new User();
+            user.userName = req.body.userName;
+            user.password = req.body.password;
+            if(req.body.nickName)
+                user.nickName = req.body.nickname
+            
+            user.save(function(err){
+                if(err)
+                    res.send(err);
+                res.json(user);
+            });
         });
     })
     .get(function(req,res){
-        
+        User.find(function(err, users){
+			if(err)
+				res.send(err);
+			res.json(users);
+		});
     });
 
 
